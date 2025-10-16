cc2cc49f709a43631a1fec0eff3d0bf06ce6414f
marinimarco000 <marini.marco@franchettisalviani.net>
2025-10-16 10:33:41 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Net.Http.Headers;
+
+namespace fattoriale
+{
+    internal class Program
+    {
+        static int fattoriale(int b) 
+        {
+            int fatt=1;
+            for (int i = 0;b>i ; b--)
+            {
+                fatt = fatt * b;
+            }
+            return fatt;
+        }
+
+        static void Main(string[] args)
+        {
+            int a,prod;
+            Console.WriteLine("di quale numero vuoi che ti faccia il fattoriale");
+            a = Convert.ToInt32(Console.ReadLine());
+            prod=fattoriale(a);
+            Console.WriteLine("il vettoriale del tuo numero è di " + prod);
+            
+        }
+    }
+}
+  
---
+++ README.md
@@ -0,0 +1,2 @@
+# fattoriale
+fattoriale di un numero dato dal'utente
